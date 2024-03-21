41aa38e23085fd992d0c429d674032bbabfb4273
rohanrajpal <rohan46000@gmail.com>
2024-03-21 17:39:06 +0530

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { Slider } from "bits-ui";
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+Here you go
+
+<Slider.Root max={100}>
+  <Slider.Range />
+  <Slider.Thumb />
+  <Slider.Input />
+</Slider.Root>
