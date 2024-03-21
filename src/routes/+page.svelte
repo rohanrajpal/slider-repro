2568bc1124872a66295c1fbc3c30678524fb543e
rohanrajpal <rohan46000@gmail.com>
2024-03-21 17:44:52 +0530

    add minimum bug

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-  import { Slider } from "bits-ui";
+	import { Slider } from 'bits-ui';
+
+	let value = [100];
 </script>
 
 <h1>Welcome to SvelteKit</h1>
@@ -7,8 +9,19 @@
 
 Here you go
 
-<Slider.Root max={100}>
-  <Slider.Range />
-  <Slider.Thumb />
-  <Slider.Input />
+<Slider.Root
+	max={1000000}
+	bind:value
+	class="relative flex w-full touch-none select-none items-center"
+	let:thumbs
+>
+	<span class="bg-secondary relative h-2 w-full grow overflow-hidden rounded-full">
+		<Slider.Range class="bg-primary absolute h-full" />
+	</span>
+	{#each thumbs as thumb}
+		<Slider.Thumb
+			{thumb}
+			class="border-primary bg-background ring-offset-background focus-visible:ring-ring block h-5 w-5 rounded-full border-2 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+		/>
+	{/each}
 </Slider.Root>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,45 @@ const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				border: 'hsl(var(--border) / <alpha-value>)',
+				input: 'hsl(var(--input) / <alpha-value>)',
+				ring: 'hsl(var(--ring) / <alpha-value>)',
+				background: 'hsl(var(--background) / <alpha-value>)',
+				foreground: 'hsl(var(--foreground) / <alpha-value>)',
+				primary: {
+					DEFAULT: 'hsl(var(--primary) / <alpha-value>)',
+					500: 'hsl(var(--primary-500) / <alpha-value>)',
+					600: 'hsl(var(--primary-600) / <alpha-value>)',
+					foreground: 'hsl(var(--primary-foreground) / <alpha-value>)'
+				},
+				secondary: {
+					DEFAULT: 'hsl(var(--secondary) / <alpha-value>)',
+					foreground: 'hsl(var(--secondary-foreground) / <alpha-value>)'
+				},
+				destructive: {
+					DEFAULT: 'hsl(var(--destructive) / <alpha-value>)',
+					foreground: 'hsl(var(--destructive-foreground) / <alpha-value>)'
+				},
+				muted: {
+					DEFAULT: 'hsl(var(--muted) / <alpha-value>)',
+					foreground: 'hsl(var(--muted-foreground) / <alpha-value>)'
+				},
+				accent: {
+					DEFAULT: 'hsl(var(--accent) / <alpha-value>)',
+					foreground: 'hsl(var(--accent-foreground) / <alpha-value>)'
+				},
+				popover: {
+					DEFAULT: 'hsl(var(--popover) / <alpha-value>)',
+					foreground: 'hsl(var(--popover-foreground) / <alpha-value>)'
+				},
+				card: {
+					DEFAULT: 'hsl(var(--card) / <alpha-value>)',
+					foreground: 'hsl(var(--card-foreground) / <alpha-value>)'
+				}
+			}
+		}
 	},
 
 	plugins: []
